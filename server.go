9ee3c5d13c2a42102b8a8ac57b06a6ed15c0a156
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-11-22 11:43:16 -0600

    post what youre thankful for

--- server.go
+++ server.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -27,4 +30,18 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	thankfulFor := r.FormValue("thankfulFor")
+
+	fmt.Fprintf(w, "\n You are %s\n", name)
+	fmt.Fprintf(w, "You're thankful for %s\n", thankfulFor)
+	fmt.Fprintf(w, "Thank you for submiting!")
 }
